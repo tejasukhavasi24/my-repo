e80abebdcffa1d64a5124e0cd6b939cfa970a7c0
Teja Sukhavasi <tejasukhavasi@Tejas-MacBook-Pro.local>
2018-03-13 01:20:23 -0700

    modified file

--- config.py
+++ config.py
@@ -7,3 +7,6 @@ api_key = 'bVWaO8ITfOLDgGDYIK3lnGJWyvEeLty05bmVI2bqib2LPNDeA9BHGIuGH0SPHhI5'
 api_secret = '23H1IfFZ4ENectSZsNpfQRwuDpoESWJdp48aBNN8YE5TpcijZJRxJhHUzqTYxnzT'
 
 recv_window = 6000000
+
+
+ADDING
