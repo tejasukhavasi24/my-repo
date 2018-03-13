382858c36667a85470f469f97889846a2265c0af
Teja Sukhavasi <tejasukhavasi@Tejas-MacBook-Pro.local>
2018-03-13 00:58:47 -0700

    new file commiting

---
+++ config.py
@@ -0,0 +1,9 @@
+# @yasinkuyu
+
+# Get an Key and Secret 
+# https://www.kucoin.com/#/user/setting/api
+
+api_key = 'bVWaO8ITfOLDgGDYIK3lnGJWyvEeLty05bmVI2bqib2LPNDeA9BHGIuGH0SPHhI5'
+api_secret = '23H1IfFZ4ENectSZsNpfQRwuDpoESWJdp48aBNN8YE5TpcijZJRxJhHUzqTYxnzT'
+
+recv_window = 6000000
