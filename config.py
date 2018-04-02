1ced36624e994fb96aef87c00cac78159a3be993
Teja Sukhavasi <tejasukhavasi@Tejas-MBP.fios-router.home>
2018-04-01 23:23:18 -0700

    modified

--- config.py
+++ config.py
@@ -1,10 +1,1 @@
-# @yasinkuyu
-
-# Get an Key and Secret 
-# https://www.kucoin.com/#/user/setting/api
-
-api_key = 'bVWaO8ITfOLDgGDYIK3lnGJWyvEeLty05bmVI2bqib2LPNDeA9BHGIuGH0SPHhI5'
-api_secret = '23H1IfFZ4ENectSZsNpfQRwuDpoESWJdp48aBNN8YE5TpcijZJRxJhHUzqTYxnzT'
-
-recv_window = 60000000
-
+this is header
